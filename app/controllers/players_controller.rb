b191b19d45f7506c9283d95dda6976a652c252fe
damating <paula.grzesiak@gmail.com>
2015-04-03 22:15:26 +0200

    Add a gravatar

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -10,6 +10,7 @@ class PlayersController < ApplicationController
   # GET /players/1
   # GET /players/1.json
   def show
+    @player = Player.find(params[:id])
   end
 
   # GET /players/new
--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,2 +1,10 @@
 module PlayersHelper
+
+  # Returns the Gravatar for the given player
+  def gravatar_for(player)
+    gravatar_id = player.id
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: player.first_name, class: "gravatar")
+  end
+
 end
