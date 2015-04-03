5559e646458d309f12b9c787bca95efa48a2e30b
damating <paula.grzesiak@gmail.com>
2015-04-03 12:50:34 +0200

    Tests and validation for the Player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,7 @@
 class Player < ActiveRecord::Base
+  validates :first_name, presence: true, length: { maximum: 50 } 
+  validates :last_name,  presence: true, length: { maximum: 50 }
+
   has_many :matches
   has_many :goals
 end
--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -4,4 +4,33 @@ class PlayerTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+ 
+ def setup
+   @player = Player.new(first_name: "Example Player", last_name: "Example Player                        ", avatar: "http://exampleurl.pl")
+ end
+
+ test "should be valid" do
+   assert @player.valid?
+ end
+
+ test "first name should be present" do
+   @player.first_name = "     "
+   assert_not @player.valid?
+ end
+
+ test "last name should be present" do
+   @player.last_name = "    "
+   assert_not @player.valid?
+ end
+
+ test "first name should not be too long" do
+   @player.first_name = "a" * 51
+   assert_not @player.valid?
+ end
+
+ test "last name should not be too long" do
+   @player.last_name = "a" * 51
+   assert_not @player.valid?
+ end
+ 
 end
